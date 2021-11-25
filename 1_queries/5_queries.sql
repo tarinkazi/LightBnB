af4f72dc54cbd7a71cb92f7fee90854d3acdc185
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-25 01:54:38 -0500

    queries

---
+++ 1_queries/5_queries.sql
@@ -0,0 +1,26 @@
+-- SELECT reservations.id, properties.id, AVG(property_reviews.rating) AS average_rating
+-- FROM reservations
+-- JOIN properties
+-- ON reservations.property_id = properties.id
+-- JOIN users
+-- ON users.id = reservations.guest_id
+-- JOIN property_reviews
+-- ON property_reviews.reservation_id = reservations.id
+-- WHERE reservations.guest_id = 1
+--  AND
+-- end_date < now()::date
+-- GROUP BY reservations.id, properties.id
+-- ORDER BY reservations.start_date
+-- -- 
+-- LIMIT 10;
+
+
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
