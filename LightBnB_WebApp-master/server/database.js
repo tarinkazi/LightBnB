14b06c3fd41ea55d06196310e80a3277d5b8fc66
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-25 19:13:48 -0500

    lighthousebnb users update

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -22,7 +22,7 @@ const getUserWithEmail = function(email) {
   return pool
   .query(comm, [email])
   .then(result => result.rows[0])
-  .catch(err =>{
+  .catch(err => {
    console.log(err.message);
   })
 }
@@ -39,7 +39,7 @@ const getUserWithId = function(id) {
   return pool
   .query(comm, [id])
   .then(result => result.rows[0])
-  .catch(err =>{
+  .catch(err => {
    console.log(err.message);
   })
 }
@@ -52,11 +52,16 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  const comm =`INSERT INTO users (name, email, password)
+   values ($1, $2, $3)
+   returning *;`
+  return pool
+  .query(comm,[user.name, user.email, user.password])
+  .then(result => result.rows[0])
+  .catch(err => {
+    console.log(err.message);
+  })
+  }
 exports.addUser = addUser;
 
 /// Reservations
